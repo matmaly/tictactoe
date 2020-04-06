9ff02fd27b95b084f5b4cd585d0d9284e3d669b3
matmaly <mateuszfmaly@gmail.com>
2020-04-06 16:23:15 +0000

    Fixing the Draw option by reseting the move counter on new game and changing some var names

--- tictactoe.py
+++ tictactoe.py
@@ -2,9 +2,9 @@
 def tic_tac_toe():
     user1 = "X"
     user2 = "O"
-    full_board_counter = 0
+    move_counter = 0  
     board_not_full = True
-    won = False
+    game_over = False
     score = {"User 1":0, "User 2":0}
     board_dict = {"1":" ","2":" ","3":" ","4":" ","5":" ","6":" ","7":" ","8":" ","9":" "}
     
@@ -34,7 +34,7 @@ def tic_tac_toe():
         return user_choice
     
     # fun that checks if player has won
-    def win():
+    def win(move_counter):
         marker = ("X","O")
         for z in marker:
             if (board_dict["1"] == board_dict["2"] == board_dict["3"] == z or
@@ -51,41 +51,40 @@ def tic_tac_toe():
                     else:
                         score["User 2"] += 1
                     print(f"Score:\nUser 1: {score['User 1']}\nUser 2: {score['User 2']}")
-                    won = True
-                    return won
+                    game_over = True
+                    return game_over
+            elif move_counter == 9:
+                print("Draw!")
+                game_over = True
+                return game_over
+
 
     draw_the_board()
     while board_not_full:    
-        if won:
+        if game_over:
             new_game = input("Start a new Game? (type 'yes' or 'no' ")
             if new_game == "yes":
                 board_dict = {"1":" ","2":" ","3":" ","4":" ","5":" ","6":" ","7":" ","8":" ","9":" "}
+                move_counter = 0
             else:
                 break
         user1_choice = player_input("User 1")
         user1_choice = field_played(user1_choice, "User 1")
-        full_board_counter += 1
+        move_counter += 1
         board_dict[user1_choice] = user1
         draw_the_board()
-        
-        # If statement that breaks the loop when the board is full 
-        if full_board_counter == 9:
-            board_dict[user1_choice] = user1 # Brute force, to fix later
-            draw_the_board()
-            break
-        
-        won = win()
+        game_over = win(move_counter)
         # check if var won is true, if it is, break
-        if won:
+        if game_over:
             continue
 
         user2_choice = player_input("User 2")
         user2_choice = field_played(user2_choice,"User 2")
-        full_board_counter += 1
+        move_counter += 1
         board_dict[user2_choice] = user2
         draw_the_board()
-        won = win()
-        if won:
+        game_over = win(move_counter)
+        if game_over:
             continue
     else:
         print("Bye")
